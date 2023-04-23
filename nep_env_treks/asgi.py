697d525fb203bd628eaead3cd788f1819bbc1867
Pragati Poudel <pragati.poudel37@gmail.com>
2023-04-22 20:42:33 -0400

    First commit

---
+++ nep_env_treks/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for nep_env_treks project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nep_env_treks.settings')
+
+application = get_asgi_application()
