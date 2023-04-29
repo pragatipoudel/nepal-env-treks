679d203704def977b649ae035270ac7c743c3bda
Pragati Poudel <pragati.poudel37@gmail.com>
2023-04-29 16:24:51 -0400

    Add itinerary admin

--- packages/admin.py
+++ packages/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 
-from .models import Activity, Season, Package, Amenity
+from .models import Activity, Season, Package, Amenity, DailyItinerary, FullItinerary
+
+
+class DailyItineraryInline(admin.StackedInline):
+    model = DailyItinerary
+
+
+class FullItineraryAdmin(admin.ModelAdmin):
+    inlines = [DailyItineraryInline]
+
 
 admin.site.register(Activity)
 admin.site.register(Season)
 admin.site.register(Package)
 admin.site.register(Amenity)
+admin.site.register(FullItinerary, FullItineraryAdmin)
